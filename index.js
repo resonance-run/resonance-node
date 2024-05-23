aa243d0b6a9994f335159c2956ba8ae38fea4a6c
Steven Foote <steven@resonance.run>
2024-05-23 13:56:29 -0600

    Move i18next data conversion to resonance-client-cache

--- index.js
+++ index.js
@@ -1,55 +1,20 @@
-export const loadCustomizations = async (type, userData, clientId, apiKey) => {
+export const loadCustomizations = async (type, integration, userData) => {
   try {
-    const baseUrl = `http://localhost:3000`;
-    const formData = new FormData();
-    formData.append(
-      'body',
-      JSON.stringify({
-        customizationType: type,
-        userData,
-        clientId,
-        apiKey,
-      })
-    );
-    const res = await fetch(`${baseUrl}/api/getCustomizations`, {
-      method: 'POST',
-      body: formData,
-    });
+    const baseUrl = `http://localhost:4500`;
+    const encodedUserData = encodeURIComponent(JSON.stringify(userData));
+    const fullUrl = `${baseUrl}/customizations?userData=${encodedUserData}&customizationType=${type}&integration=${integration}`;
+    const res = await fetch(fullUrl);
     const data = await res.json();
     return data;
   } catch (error) {
+    console.log('We had an error');
     // In case of an error, return an empty object instead of killing everything.
     console.error(error);
     return {};
   }
 };
 
-export const loadCustomizationDataForI18Next = async (type, userData, clientId, apiKey) => {
-  const customizationData = await loadCustomizations(type, userData, clientId, apiKey);
-  const entries = Object.entries(customizationData);
-  return entries.reduce((res, [key, customization]) => {
-    const lang = customization.locale || 'en';
-    const [namespace, stringKey] = key.split(':');
-    if (!res[lang]) {
-      res[lang] = {};
-    }
-    if (!res[lang][namespace]) {
-      res[lang][namespace] = {};
-    }
-    const keyParts = stringKey.split('.');
-    let nextPart;
-    let obj = res[lang][namespace];
-    while (keyParts.length) {
-      nextPart = keyParts.shift();
-      const isLastPart = keyParts.length === 0;
-      if (!isLastPart) {
-        if (!obj[nextPart]) {
-          obj[nextPart] = {};
-        }
-        obj = obj[nextPart];
-      }
-    }
-    obj[nextPart] = customization.variation.fields.stringValue.value;
-    return res;
-  }, {});
+export const loadCustomizationDataForI18Next = async (type, userData) => {
+  const customizationData = await loadCustomizations(type, 'i18next', userData);
+  return customizationData;
 };
