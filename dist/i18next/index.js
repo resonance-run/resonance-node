7d58b4bd6f333bb297036f56706ffba5a63ab9aa
Steven Foote <steven@resonance.run>
2024-06-14 22:04:49 -0400

    Handle control in i18next

--- dist/i18next/index.js
+++ dist/i18next/index.js
@@ -9,20 +9,22 @@ export const getCustomizationsFori18next = (customizationData) => {
         if (!res[lang][namespace]) {
             res[lang][namespace] = {};
         }
-        const keyParts = stringKey.split('.');
-        let nextPart;
-        let obj = res[lang][namespace];
-        while (keyParts.length) {
-            nextPart = keyParts.shift();
-            const isLastPart = keyParts.length === 0;
-            if (!isLastPart) {
-                if (!obj[nextPart]) {
-                    obj[nextPart] = {};
+        if (customization.variation.id !== 'control') {
+            const keyParts = stringKey.split('.');
+            let nextPart;
+            let obj = res[lang][namespace];
+            while (keyParts.length) {
+                nextPart = keyParts.shift();
+                const isLastPart = keyParts.length === 0;
+                if (!isLastPart) {
+                    if (!obj[nextPart]) {
+                        obj[nextPart] = {};
+                    }
+                    obj = obj[nextPart];
                 }
-                obj = obj[nextPart];
             }
+            obj[nextPart] = customization.variation.fields.stringValue.value;
         }
-        obj[nextPart] = customization.variation.fields.stringValue.value;
         return res;
     }, {});
 };
